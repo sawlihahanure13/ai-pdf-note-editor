1e73651390b3909a28e9d778c520d36056283e5a
sawliha1 <sawlihahanure16@gmail.com>
2025-09-22 18:33:27 +0530

    Altered Changes

--- convex/fileStorage.js
+++ convex/fileStorage.js
@@ -1,68 +1,66 @@
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
+// Generate upload URL for a file
 export const generateUploadUrl = mutation(async (ctx) => {
-    return await ctx.storage.generateUploadUrl();
+  return await ctx.storage.generateUploadUrl();
 });
 
+// Add new file entry in DB
 export const AddFileEntryToDb = mutation({
-    args: {
-        fileId: v.string(),
-        storageId: v.string(),
-        fileName: v.string(),
-        fileUrl: v.string(),
-        createdBy: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const result = await ctx.db.insert("pdfFiles", {
-            fileId: args.fileId,
-            storageId: args.storageId,
-            fileName: args.fileName,
-            fileUrl: args.fileUrl,
-            createdBy: args.createdBy,
-        });
-        return "Inserted";
-    },
+  args: {
+    fileId: v.string(),
+    storageId: v.string(),
+    fileName: v.string(),
+    fileUrl: v.string(),
+    userEmail: v.string(),
+  },
+  handler: async (ctx, args) => {
+    await ctx.db.insert("pdfFiles", {
+      fileId: args.fileId,
+      storageId: args.storageId,
+      fileName: args.fileName,
+      fileUrl: args.fileUrl,
+      userEmail: args.userEmail,
+    });
+    return "Inserted";
+  },
 });
 
+// Get file URL by storageId
 export const getFileUrl = mutation({
-    args: {
-        storageId: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const url = ctx.storage.getUrl(args.storageId);
-        return url;
-    },
+  args: {
+    storageId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return ctx.storage.getUrl(args.storageId);
+  },
 });
 
+// Get file record by fileId
 export const getFileRecord = query({
-    args: {
-        fileId: v.string(),
-    },
-    handler: async (ctx, args) => {
-        const result = await ctx.db
-            .query("pdfFiles")
-            .filter((q) => q.eq(q.field("fileId"), args.fileId))
-            .collect();
-        console.log(result);
-        return result;
-    },
+  args: {
+    fileId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    return await ctx.db
+      .query("pdfFiles")
+      .filter((q) => q.eq(q.field("fileId"), args.fileId))
+      .collect();
+  },
 });
 
+// Get all files for a user
 export const getUserFiles = query({
-    args: {
-        userEmail: v.optional(v.string()),
-    },
-    handler: async (ctx, args) => {
-        if(!args.userEmail) {
-            return;
-        }
-        const result = await ctx.db
-            .query("pdfFiles")
-            .filter((q) => q.eq(q.field("createdBy"), args.userEmail))
-            .collect();
+  args: {
+    userEmail: v.optional(v.string()),
+  },
+  handler: async (ctx, { userEmail }) => {
+    if (!userEmail) return [];
 
-        return result;
-    },
+    return await ctx.db
+      .query("pdfFiles")
+      .filter((q) => q.eq(q.field("userEmail"), userEmail))
+      .collect();
+  },
 });
